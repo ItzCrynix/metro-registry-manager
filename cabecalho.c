69071aa94179f5945bddc7f25c8dadb41ee56cfd
ItzCrynix <cainanloyola376@gmail.com>
2026-03-25 00:00:58 -0300

    moved Cabecalho struct to a separate file

---
+++ cabecalho.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "cabecalho.h"
+
+Cabecalho* novo_cabecalho() {
+    Cabecalho* novo = (Cabecalho*) malloc(sizeof(Cabecalho));
+    if (novo == NULL) {
+        return NULL;
+    }
+
+    // escreve os valores padrões que o cabeçalho deve assumir em um arquivo vazio
+    novo->status = STATUS_CONSISTENT;
+    novo->topo_pilha = -1;
+    novo->proximo_rrn = 0;
+    novo->numero_estacoes = 0;
+    novo->numero_pares_estacoes = 0;
+
+    return novo;
+}
+
+void salvar_cabecalho(FILE* arquivo_binario, Cabecalho* cabecalho_binario) {
+    // volta o ponteiro para o inicio do arquivo
+    rewind(arquivo_binario);
+
+    // Escreve os registros de cabeçalho
+    fwrite(&cabecalho_binario->status, sizeof(char), 1, arquivo_binario);
+    fwrite(&cabecalho_binario->topo_pilha, sizeof(int), 1, arquivo_binario);
+    fwrite(&cabecalho_binario->proximo_rrn, sizeof(int), 1, arquivo_binario);
+    fwrite(&cabecalho_binario->numero_estacoes, sizeof(int), 1, arquivo_binario);
+    fwrite(&cabecalho_binario->numero_pares_estacoes, sizeof(int), 1, arquivo_binario);
+}
---
+++ cabecalho.h
@@ -0,0 +1,31 @@
+#ifndef CABECALHO_H
+#define CABECALHO_H
+
+#include <stdio.h>
+
+#define TAM_REGISTRO_CABECALHO 17
+#define STATUS_CONSISTENT '0'
+#define STATUS_INCONSISTENT '1'
+
+typedef struct cabecalho {
+    char status;
+    int topo_pilha;
+    int proximo_rrn;
+    int numero_estacoes;
+    int numero_pares_estacoes;
+} Cabecalho;
+
+/**
+ * @brief Aloca memória para um novo cabeçalho e define valores padrões pra ele
+ * @return Ponteiro para um cabeçalho ou NULL
+ */
+Cabecalho* novo_cabecalho();
+
+/**
+ * @brief Salva as informações contidas no cabeçalho de um arquivo binário
+ * @param arquivo_binario O arquivo que vai ser manipulado
+ * @param cabecalho_binario O cabeçalho que contem as informações que vão ser salvas
+ */
+void salvar_cabecalho(FILE* arquivo_binario, Cabecalho* cabecalho_binario);
+
+#endif
