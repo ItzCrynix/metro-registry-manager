6a9e3ac4bfe56dd33b88adba4cd32677372b1a96
ItzCrynix <cainanloyola376@gmail.com>
2026-03-30 15:00:29 -0300

    moved rrn search function to registro

--- funcionalidades.h
+++ funcionalidades.h
@@ -15,8 +15,6 @@
 #define FILE_NOT_FOUND_ERROR -1
 #define MALLOC_ERROR -2
 #define NO_DATA_FOUND_ERROR -3
-#define RRN_NOT_FOUND -3
-#define RRN_REMOVED '1'
 
 /**
  * @brief Escreve um registro novo no arquivo
@@ -26,17 +24,11 @@
 int escrever_registros_csv(FILE* nome_arquivo_csv, FILE* nome_arquivo_binario);
 
 /**
- * @brief Lê um registro guardado no arquivo binário baseado no RRN (Relative Record Number)
- * @param nome_arquivo nome do arquivo binário que será usado
- * @param RRN Relative Record Number, indica qual "linha" dos registros vai ser lida
- * @return Um registro, caso encontre, ou NULL
- */
-Registro* ler_registro_RRN(FILE* nome_arquivo, int RRN);
-
-/**
  * @brief Lê um arquivo binário e printa no terminal
  * @param arquivo_binario O arquivo que será lido
  */
 int printar_arquivo_binario(FILE* arquivo_binario);
+
+int procurar_registro_RRN(FILE* arquivo_binario, Registro** registro, int rrn);
 
 #endif
--- registro.h
+++ registro.h
@@ -5,8 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utils.h"
+#include "cabecalho.h"
 
 #define TAM_REGISTRO_DADOS 80
+
+#define STATUS_NOT_REMOVED '0'
+#define STATUS_REMOVED '1'
 
 typedef struct registro {
     char removido;
@@ -45,7 +49,17 @@ char* to_string(Registro* registro);
 
 /**
  * @brief Salva as informações de um registro em um arquivo binário
+ * @param arquivo_binario
+ * @param novo_registro O registro que será salvo
  */
 void salvar_registro_binario(FILE* arquivo_binario, Registro* novo_registro);
+
+/**
+ * @brief Lê um registro guardado no arquivo binário baseado no RRN (Relative Record Number)
+ * @param nome_arquivo nome do arquivo binário que será usado
+ * @param RRN Relative Record Number, indica qual "linha" dos registros vai ser lida
+ * @return Um registro, caso encontre, ou NULL
+ */
+Registro* ler_registro_RRN(FILE* nome_arquivo, int RRN);
 
 #endif
