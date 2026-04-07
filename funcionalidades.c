4688e0c7b3de7aacb9b8b6964e66b3ca8d871df1
ItzCrynix <cainanloyola376@gmail.com>
2026-04-06 22:56:49 -0300

    Implemented station count

--- funcionalidades.c
+++ funcionalidades.c
@@ -32,11 +32,7 @@ int busca_estacao(char** estacoes, int tam, char* procurado) {
 }
 
 int escrever_registros_csv(FILE* arquivo_csv, FILE* arquivo_binario) {
-    if (arquivo_binario == NULL) {
-        return FILE_NOT_FOUND_ERROR;
-    }
-
-    if (arquivo_csv == NULL) {
+    if (arquivo_binario == NULL || arquivo_csv == NULL) {
         return FILE_NOT_FOUND_ERROR;
     }
 
